484fea962e6bda1070d634856dda8e5458c179d8
Reem Waleed <reem.waleed.ahmed@gmail.com>
2025-08-25 00:24:43 +0300

    Initial commit

--- Backend_foodo/homecook_backend/serializers.py
+++
@@ -1,9 +0,0 @@
-# الموقع: D:\Backend_foodo\homecook_backend\serializers.py
-
-from rest_framework import serializers
-from .models import Order
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
--- Backend_foodo/homecook_backend/views.py
+++
@@ -1,9 +0,0 @@
-# الموقع: D:\Backend_foodo\homecook_backend\views.py
-
-from rest_framework import viewsets
-from homecook_backend.models import Order  # استيراد من models.py
-from homecook_backend.serializers import OrderSerializer  # استيراد من serializers.py
-
-class OrderViewSet(viewsets.ModelViewSet):
-    queryset = Order.objects.all()  # هذا السطر مهم
-    serializer_class = OrderSerializer
